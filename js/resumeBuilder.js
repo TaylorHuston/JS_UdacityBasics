a2a811cd9c594c484409f1153e9f0eeb67d041f2
Taylor Huston <taylor.a.huston@gmail.com>
2015-02-19 02:03:47 -0700

    Complete lesson 2

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,24 @@
-$("#main").append("Taylor Huston");
+
+var bio = {
+	"Name" : "Taylor Huston",
+	"Age" : "30",
+	"Role" : "Aspiring Web Developer",
+	"Profile" : "images/me.jpg",
+	"Skills" : ["Java","JavaScript","HTML/CSS"]
+
+};
+
+var education = {
+	"School" : "University of Advancing Technology",
+	"Degree" : "B.A. in Game Design"
+}
+
+var work = {
+	"Company" : "TherapySites",
+	"Role" : "Front End Developer"
+}
+
+var formattedName = HTMLheaderName.replace("%data%",bio.Name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.Role);
+
+$("#header").prepend(formattedRole).prepend(formattedName);
